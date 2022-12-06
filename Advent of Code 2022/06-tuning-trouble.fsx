f1fb2bc960851073c5aad601658ca35a460712d5
Samuli Mäki <samuli.maki@gofore.com>
2022-12-06 10:36:35 +0200

    day 6

---
+++ Advent of Code 2022/06-tuning-trouble.fsx
@@ -0,0 +1,19 @@
+﻿#load "Utils.fsx"
+open Utils
+
+let customSubString len (str : string) endIndex =
+    str[(endIndex - len + 1)..(endIndex)]
+    
+let findStartOfPacketMarker str markerSize =
+    let subStr = customSubString markerSize str
+
+    let rec findIt index =
+        match subStr index |> Set.ofSeq |> Set.count = markerSize with
+        | true -> index + 1 
+        | false -> findIt (index + 1)
+    findIt (markerSize - 1)
+
+let input = IO.readFileAsString "06-tuning-trouble.txt"
+
+findStartOfPacketMarker input 4
+findStartOfPacketMarker input 14
