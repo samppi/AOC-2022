14b25ff11fccbeb6654b82d97eb5460fbe2e4590
Samuli Mäki <samuli.maki@gofore.com>
2022-12-31 22:01:48 +0200

    day 20

--- Advent of Code 2022/20-grove-positioning-system.fsx
+++ Advent of Code 2022/20-grove-positioning-system.fsx
@@ -1,42 +1,38 @@
 ﻿#load "Utils.fsx"
 open Utils
 
-let findNewIndex (arr: List<int>) i (amount: int64) =
-    let newIndex = int ( (i + amount) % int64 (arr.Length - 1))
-    if newIndex <= 0 then arr.Length + newIndex - 1
+let findNewIndex (lst: int array) i (moveBy: int64) =
+    let newIndex = int ( (i + moveBy) % int64 (lst.Length - 1))
+    if newIndex <= 0 then lst.Length + newIndex - 1
     else newIndex
 
 let mixNumbers (input: int64 array) times =
     let mixed = input |> Array.mapi (fun i x -> (i,x))
     
-    let mutable arr = List.init (Array.length input) (fun i -> i)
+    let mutable arr = Array.init (Array.length input) (fun i -> i)
     for _ in 1..times do
         mixed
         |> Array.iter(
             fun (origIndex, amount) ->
-                let curIndex = arr |> List.findIndex (fun x -> x = origIndex)
+                let curIndex = arr |> Array.findIndex (fun x -> x = origIndex)
                 let newIndex = findNewIndex arr curIndex amount
             
                 let oldItem = arr.[curIndex]
-                arr <- arr |> List.removeAt curIndex |> List.insertAt newIndex oldItem
+                arr <- arr |> Array.removeAt curIndex |> Array.insertAt newIndex oldItem
                 
         )    
     arr
 
-let solve (intInput: int64 array) (decryptionKey: int64) times = 
-
+let solve (intInput: int64 array) (decryptionKey: int64) times =
     let decrypted = intInput |> Array.map (fun x -> x * decryptionKey)
     let res  = mixNumbers decrypted times
-
-    let getNormzliedIndexd (inp: int64 array) res =
-        let oriuginalZi = inp |> Array.findIndex (fun x -> x = 0)
-        res |> List.findIndex (fun x -> x = oriuginalZi)
-  
-    let zi = getNormzliedIndexd  decrypted res
-
+    
+    let origZero = intInput |> Array.findIndex (fun x -> x = 0)        
+    let zeroIndex = res |> Array.findIndex (fun x -> x = origZero)
+    
     [1000;2000;3000]
-    |> List.map (fun i -> (i + zi) % List.length res)
-    |> List.map (fun i -> List.item i res)
+    |> List.map (fun i -> (i + zeroIndex) % Array.length res)
+    |> List.map (fun i -> Array.item i res)
     |> List.map (fun i -> decrypted[i])
     |> List.sum
 
@@ -44,5 +40,6 @@ let intInput = IO.readLines "20-grove-positioning-system.txt" |> Seq.map int64 |
 
 // First part
 solve intInput 1 1
+
 // Second part
 solve intInput 811589153L 10
